bb9011a7d7c2205ce386f5c7b953f72a4b8e8033
PixelsTownTechnologies <pixelstown@hotmail.com>
2020-07-06 21:55:54 +0300

    Add static root

--- settings/base.py
+++ settings/base.py
@@ -105,6 +105,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
