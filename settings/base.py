fbbcf8426570317edb7c8e99629bff1e23cd81e8
PixelsTownTechnologies <pixelstown@hotmail.com>
2020-07-06 23:17:45 +0300

    Add static root

--- settings/base.py
+++ settings/base.py
@@ -8,11 +8,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
+Access_Control_Allow_Credentials = True
 CORS_ORIGIN_WHITELIST = [
     'http://google.com',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://cakefe.herokuapp.com/',
+    'https://cakefe.herokuapp.com',
     'cakefe.herokuapp.com'
 ]
 INSTALLED_APPS = [
