123158cf777e3df21efc1c4c71881ec555be50ae
PixelsTownTechnologies <pixelstown@hotmail.com>
2020-07-06 23:04:17 +0300

    add FE

--- settings/base.py
+++ settings/base.py
@@ -11,7 +11,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     'http://google.com',
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://cakefe.herokuapp.com/'
 ]
 INSTALLED_APPS = [
     'django.contrib.admin',
