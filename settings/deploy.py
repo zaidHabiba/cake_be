aeb3fc9a8f0bfc7566065ebcff552b539a50d091
PixelsTownTechnologies <pixelstown@hotmail.com>
2020-07-06 21:42:28 +0300

    edit settings deploy

--- settings/deploy.py
+++ settings/deploy.py
@@ -1,3 +1,19 @@
 from settings.base import *
+import dj_database_url
 import django_heroku
+
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
 django_heroku.settings(locals())
